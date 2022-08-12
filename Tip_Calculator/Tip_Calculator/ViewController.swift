004d3deb2e609d28064795de027327078189241b
Efren Ramirez <78672241+efrenRamirez5@users.noreply.github.com>
2022-08-12 11:27:15 -0700

    Add files via upload

---
+++ Tip_Calculator/Tip_Calculator/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Tip_Calculator
+//
+//  Created by Efren Ramirez Rojas on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet var background: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // Makes bill amount first responder
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.billAmountTextField.becomeFirstResponder()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        //let defaults = UserDefaults.standard
+        
+//        if (SettingsViewController.background.backgroundColor == UIColor.black) {
+//            background.backgroundColor = UIColor.black
+//        }
+        
+        
+//        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//
+//        let
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    
+
+    @IBAction func calculateTip(_ sender: Any) {
+//        Picker("Tip percentage", selection: $tipPercentage) {
+//            ForEach(tipPercentages, id: \.self) {
+//                Text($0, format: .percent)
+//            }
+//        }
+        
+        // Get bill amount from text field imput
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Get total tip by multiplying tip * tipPercentage
+        let tipPercentages = [0.10, 0.15, 0.20]
+        let tip = bill *
+            tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        // Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
